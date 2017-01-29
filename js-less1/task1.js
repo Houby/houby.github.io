3ac5aa0ef395d4f405ef93e85fdf423d83f65ee1
Houby <by>
2017-01-30 01:39:33 +0300

    mupp

---
+++ js-less1/task1.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Yakuhaishik on 30.01.2017.
+ */
+/*
+
+
+var age = prompt('сколько вам лет', 18);
+
+confirm('Вам ' + age + ' лет.');
+*/
+
+var num = 1, huyam=5;
+
+
+console.log('huyam ', huyam);
+
+alert(''&&1);
