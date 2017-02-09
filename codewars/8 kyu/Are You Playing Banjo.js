7d2af4f38c9ff1095459c5ae28d642baf53af57c
Houby <by>
2017-02-09 03:44:05 +0300

    upp

---
+++ codewars/8 kyu/Are You Playing Banjo.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Yakuhaishik on 09.02.2017.
+ */
+function areYouPlayingBanjo(name) {
+    if (name.startsWith('r') || name.startsWith('R')) {
+        return name + " plays banjo";}
+    else {return name + " does not play banjo";}
+};
---
+++ codewars/8 kyu/Plural.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Yakuhaishik on 09.02.2017.
+ */
+function plural(n) {
+    if (n==1) {return false;}
+    else {return true;};
+};
